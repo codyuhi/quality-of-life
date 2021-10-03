ce55eaa56b9813d124a9e2bc70f35aa8e402f204
Cody Uhi <codylkuhi@gmail.com>
2021-10-03 16:57:28 -0600

    Built city list. Needs polish

--- react-app/src/components/Content.js
+++ react-app/src/components/Content.js
@@ -2,50 +2,79 @@ import React from 'react';
 import 'App.css';
 
 export default class Content extends React.Component {
-    setSearchTerm = () => {
-        this.props.updateSearchTerm((searchTerm) => 'Content button clicked')
+    setSearchTerm = (e) => {
+        this.props.updateSearchTerm((searchTerm) => e.target.value)
     }
-    getCityInfo = () => {
-        this.props.getCityInfo(this.props.cityList._embedded['city:search-results'][0]._links['city:item'].href)
+    checkEnterPressed = (e) => {
+        if (e.keyCode === 13) {
+            this.props.search()
+        }
+    }
+    getCityInfo = (index) => {
+        this.props.getCityInfo(this.props.cityList._embedded['city:search-results'][index]._links['city:item'].href)
     }
     render() {
+        const hero = (
+            <div className="Hero-Container">
+                <h1><i className="fa fa-heartbeat"> </i> Quality of Life</h1>
+                <p>This application allows you to see several metrics regarding a city's quality of living.</p>
+                <p>Use the search bar below or at the top to search for a city that you would like more information about.</p>
+                <input
+                    autoFocus
+                    text="text"
+                    value={this.props.searchTerm}
+                    onChange={this.setSearchTerm}
+                    placeholder="Search"
+                    onKeyDown={this.checkEnterPressed}></input>
+                <button>Search</button>
+            </div>
+        );
+        const cityListDivs = [];
+        if (this.props.cityList) {
+            for (let i = 0; i < this.props.cityList._embedded['city:search-results'].length; i++) {
+                cityListDivs.push(
+                    <div key={'city-list-' + i.toString()} className="City-List-Item" onClick={this.getCityInfo.bind(this, i)}>
+                        {this.props.cityList._embedded['city:search-results'][i].matching_full_name}
+                    </div>
+                )
+            }
+        }
         const cityList = (
-            this.props.cityList ?
-                <p>{JSON.stringify(this.props.cityList)}</p> :
-                <p>No City List</p>);
+            <div className="City-List-Container">
+                <h1>Select the Desired City:</h1>
+                <div className="List-Container">
+                    {cityListDivs}
+                </div>
+            </div>
+        );
         const activeCity = (
             this.props.activeCity ?
                 <p>{JSON.stringify(this.props.activeCity)}</p> :
                 <p>No City Selected</p>
         );
 
-        const cityImg = (
-            this.props.cityImg ?
-                <p>{JSON.stringify(this.props.cityImg)}</p> :
-                <p>There is no city img</p>
-        );
+        // const cityImg = (
+        //     this.props.cityImg ?
+        //         <p>{JSON.stringify(this.props.cityImg)}</p> :
+        //         <p>There is no city img</p>
+        // );
 
-        const advancedCityData = (
-            this.props.advancedCityData ?
-                <p>{JSON.stringify(this.props.advancedCityData)}</p> :
-                <p>No Advanced City Data on file</p>
-        );
+        // const advancedCityData = (
+        //     this.props.advancedCityData ?
+        //         <p>{JSON.stringify(this.props.advancedCityData)}</p> :
+        //         <p>No Advanced City Data on file</p>
+        // );
 
         return (
             <div className="Content-Container">
                 <div className="Content center">
-                    <h1>Content</h1>
-                    <p>{this.props.searchTerm}</p>
-                    <h2>City List:</h2>
-                    {cityList}
-                    <h2>City Data:</h2>
-                    {activeCity}
-                    <h2>City Image:</h2>
-                    {cityImg}
-                    <h2>Advanced City Data</h2>
-                    {advancedCityData}
-                    <button onClick={this.setSearchTerm}>Change search term</button>
-                    <button onClick={this.getCityInfo}>Get data for first city</button>
+                    {
+                        this.props.cityList ?
+                            this.props.activeCity ?
+                                activeCity :
+                                cityList :
+                            hero
+                    }
                 </div>
             </div>
         )
